1d2ce7c6dcf4843894f2391d16b516f9fd9e6694
ZoranUTF8 <janjiczoran23@gmail.com>
2023-03-29 22:42:28 +0900

    add lint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    node: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: "airbnb-base",
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: { quotes: ["error", "double"], indent: ["error", 2] },
+};
