ed6b6b70623f4e6b85186e9f09c2c395cfee7742
ZoranUTF8 <janjiczoran23@gmail.com>
2023-04-27 11:23:20 +0900

    add custom validators to Tours model.

--- models/Tour.js
+++ models/Tour.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
+const validator = require("validator");
 
 const tourSchema = new mongoose.Schema(
   {
@@ -7,6 +9,12 @@ const tourSchema = new mongoose.Schema(
       trim: true,
       required: [true, "Tour must have a name."],
       unique: true,
+      maxLength: [
+        50,
+        "Tour name must have less than or equal then 50 characters.",
+      ],
+      minLength: [10, "Tour name must be at least 10 characters."],
+      validate: [validator.isAlpha, "Tour name can only contain characters."],
     },
     description: {
       type: String,
@@ -16,6 +24,7 @@ const tourSchema = new mongoose.Schema(
     price: {
       type: Number,
       required: [true, "Tour must have a price."],
+      min: [0, "Price must be at least 0."],
     },
     imageCover: {
       type: String,
@@ -31,16 +40,32 @@ const tourSchema = new mongoose.Schema(
         message: "Tour must have between 1 and 5 images",
       },
     },
-    discountPrice: { type: Number },
+    discountPrice: {
+      type: Number,
+      validate: {
+        validator(value) {
+          return value < this.price;
+        },
+        message:
+          "Tour discount must be less or equal to the tour regular price.",
+      },
+    },
     ratingAverage: {
       type: Number,
-      default: 0.0,
+      default: 0,
+      min: [0, "Rating must be between 0 and 5"],
+      max: [5, "Rating must be between 0 and 5"],
     },
     ratingQuantity: {
       type: Number,
       default: 0,
+      min: [0, "Rating must be at least 0."],
     },
-    duration: { type: Number, required: [true, "Tour must have a duration."] },
+    duration: {
+      type: Number,
+      required: [true, "Tour must have a duration."],
+      min: [0, "Duration must be at least 0."],
+    },
     maxGroupSize: {
       type: Number,
       required: [true, "Tour must have a Group Size"],
@@ -48,6 +73,10 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, "Tour must have a difficulty."],
+      enum: {
+        values: ["easy", "medium", "hard"],
+        message: "Difficulty must be either: easy, medium or hard",
+      },
     },
     summary: {
       type: String,
@@ -58,9 +87,59 @@ const tourSchema = new mongoose.Schema(
       type: [Date],
       required: [true, "Tour must have start dates."],
     },
+    // * Private custom tours
+    privateTour: {
+      type: Boolean,
+      default: false,
+    },
+    slug: {
+      type: String,
+    },
   },
-  { timestamps: true }
+  { timestamps: true, toJSON: { virtuals: true }, toObject: { virtuals: true } }
 );
+
+// * Virtual properties
+/*
+  virtual properties are fields that are not stored in the database,
+  but are calculated on-the-fly based on other fields in the document.
+  Virtual properties can be useful for adding additional computed properties
+  to a document without actually storing them in the database.
+  - Virtuals cannot be used in a query as they are not saved to the database but
+   made on the database query
+*/
+
+tourSchema.virtual("tourDurationInWeeks").get(function () {
+  return this.duration / 7;
+});
+
+// * Document middleware
+/*
+allows you to add custom logic to be executed before or after certain
+events occur on a MongoDB document. This middleware can be used to perform
+various tasks, such as validation, data transformation, or triggering other
+actions based on changes to a document.
+*/
+
+// ? Runs before the document .save() or .create()
+tourSchema.pre("save", function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// * Query middleware
+/*
+add custom logic to be executed before or after certain query methods
+are executed on a MongoDB collection. This
+*/
+
+tourSchema.pre("^find", function (next) {
+  /*
+  Dont return the tour if it is private for all the find methods
+  */
+  this.find({ privateTour: { $ne: true } });
+  next();
+});
 
 const Tour = mongoose.model("Tour", tourSchema);
 module.exports = Tour;
