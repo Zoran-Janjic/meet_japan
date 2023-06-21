c5d8f1f3ef2dc513f7745ff55ca220a811d8a95a
ZoranUTF8 <janjiczoran23@gmail.com>
2023-06-21 15:52:36 +0900

    calculating_average_rating_on_tours

--- models/Review.js
+++ models/Review.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const TourModel = require("./Tour");
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -42,6 +43,45 @@ const reviewSchema = new mongoose.Schema(
   }
 );
 
+//! Creating custom indexes only on most queried data
+/*  One review per user as the combination of a user and a review has to be unique
+This means that there cannot be multiple reviews with the same tour
+and user values. If an attempt is made to insert a duplicate review
+with the same tour and user, it will result in an error.
+*/
+reviewSchema.indexes({ tour: 1, user: 1 }, { unique: true });
+
+//* Static model functions
+// ? Get average review for a tour
+reviewSchema.statics.calculateAverageRating = async function (currentTourId) {
+  // ?Calculate review statistics using aggregation pipeline
+  const reviewStats = await this.aggregate([
+    { $match: { tour: currentTourId } },
+    {
+      $group: {
+        _id: "$tour",
+        totalRatings: { $sum: 1 },
+        averageRating: { $avg: "$rating" },
+      },
+    },
+  ]);
+
+  if (reviewStats.length > 0) {
+    // ?Update the corresponding tour document with the calculated statistics
+    await TourModel.findByIdAndUpdate(currentTourId, {
+      ratingAverage: reviewStats[0].averageRating,
+      ratingQuantity: reviewStats[0].totalRatings,
+    });
+  } else {
+    // ?Update the corresponding tour document with the calculated statistics
+    await TourModel.findByIdAndUpdate(currentTourId, {
+      ratingAverage: 0,
+      ratingQuantity: 0,
+    });
+  }
+};
+// * End of Static model functions
+
 // * Query middleware
 /*
 add custom logic to be executed before or after certain query methods
@@ -55,6 +95,29 @@ reviewSchema.pre(/^find/, function (next) {
     select: "name photo",
   });
   next();
+});
+
+// ? Once a new review has been added then we update the ratingAverage and
+// ? ratingQuantity for the tour which the review belongs to
+reviewSchema.post("save", function () {
+  this.constructor.calculateAverageRating(this.tour);
+});
+// ? Updating and deleting the review and updating the correct tour details according to the
+// ? Works for find one and update and find and delete
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  // ? Get access to the current document
+  this.currentReview = await this.clone().findOne();
+  next();
+});
+// ? We passed the data from the pre middleware to the post middleware so
+// ? we can get the the current review and the tour id
+reviewSchema.post(/^findOneAnd/, async function () {
+  // ? await this.findOne() does not work here as the query has already executed
+  if (this.currentReview) {
+    await this.currentReview.constructor.calculateAverageRating(
+      this.currentReview.tour
+    );
+  }
 });
 
 // * End of  query middleware
