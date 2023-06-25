52c7cb912349e177f5ac8854fee8a7967b7972c6
ZoranUTF8 <janjiczoran23@gmail.com>
2023-06-25 22:02:39 +0900

    add singleton db connection

--- db/connectDB.js
+++ db/connectDB.js
@@ -1,12 +1,69 @@
 const mongoose = require("mongoose");
+const colors = require("colors/safe");
 
-const connectDB = (url, dbPassword) => {
-  const connectionUrlWithPassword = url.replace("<PASSWORD>", dbPassword);
-  mongoose.set("strictQuery", false);
-  return mongoose.connect(connectionUrlWithPassword, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-};
+class DBConnectionClass {
+  static DBConnectionInstance; // ? Variable to hold the singleton instance
 
-module.exports = connectDB;
+  static DBConnectionStatus = false; // ? Flag to track connection status
+
+  constructor(uri, dbPassword) {
+    if (DBConnectionClass.DBConnectionInstance) {
+      // ? Ensure only one instance is created
+      throw new Error("Only one DBConnection instance can be created.");
+    }
+
+    if (!uri || !dbPassword) {
+      // ? Check if uri or dbPassword is undefined or empty
+      throw new Error("URI and dbPassword must be provided.");
+    }
+
+    this.connectionUrlWithPassword = uri.replace("<PASSWORD>", dbPassword);
+    // ? Freeze the instance to prevent modifications
+    DBConnectionClass.DBConnectionInstance = Object.freeze(this);
+  }
+
+  async connect() {
+    if (DBConnectionClass.DBConnectionStatus) {
+      console.log("Already connected.");
+      throw new Error("Already connected.");
+    }
+
+    console.log(
+      colors.bgYellow.bold(`Connecting to ${this.connectionUrlWithPassword}`)
+    );
+
+    mongoose.set("strictQuery", false);
+    try {
+      const conn = await mongoose.connect(this.connectionUrlWithPassword, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+
+      console.log(
+        colors.bgGreen.white.bold(
+          "DB connection successful! Starting app server..."
+        )
+      );
+      console.log(
+        colors.bgWhite.white.bold(`Current NODE_ENV ${process.env.NODE_ENV}`)
+      );
+      DBConnectionClass.DBConnectionStatus = true;
+      return conn;
+    } catch (error) {
+      console.log(`Error connecting to ${this.connectionUrlWithPassword}`);
+      throw error; // Rethrow the error to handle it elsewhere
+    }
+  }
+
+  static async disconnect() {
+    if (DBConnectionClass.DBConnectionStatus) {
+      await mongoose.disconnect();
+      console.log("Database disconnected.");
+      DBConnectionClass.DBConnectionStatus = false;
+    } else {
+      throw new Error("Not connected.");
+    }
+  }
+}
+
+module.exports = DBConnectionClass; // ? Export the DBConnectionClass as a module
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
-const connectDB = require("./db/connectDB");
+const DBConnectionClass = require("./db/connectDB");
 const app = require("./index");
 const PORT = process.env.PORT || 5000;
-
 const colors = require("colors/safe");
+
 // ? Catch error at startup
 process.on("uncaughtException", (err) => {
   console.log(`Uncaught error occurred: ${(err.name, err.message)}`);
@@ -14,28 +14,23 @@ process.on("uncaughtException", (err) => {
 
 /*
 Connect to db and than start the server if connection is established
+The following code assumes you have the necessary environment variables defined before running it.
+Create a new instance of DBConnectionClass with the provided URI and password
+
 */
-connectDB(
+
+const DatabaseConnection = new DBConnectionClass(
   process.env.ATLAS_DB_DEVELOPMENT_URI,
   process.env.ATLAS_DB_DEVELOPMENT_PASSWORD
-)
-  .then(() => {
-    console.log(
-      colors.bgGreen.white.bold(
-        "DB connection successful! Starting app server..."
-      )
-    );
-    console.log(
-      colors.bgWhite.white.bold(`Current NODE_ENV ${process.env.NODE_ENV}`)
-    );
-  })
-  .then(() => {
+);
+
+DatabaseConnection.connect()
+  .then(() =>
     app.listen(PORT, () => {
       console.log(
         colors.bgGreen.white.bold(`App running on port ${PORT}...SERVER.JS`)
       );
-    });
-  })
+    }))
   .catch((err) => {
     console.log(
       colors.bgRed.white.bold(`Error connecting to database: ${err.message}`)
