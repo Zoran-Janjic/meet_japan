306e70405ef5860ea668ad5c15e9f4099ea226d4
ZoranUTF8 <janjiczoran23@gmail.com>
2023-11-12 13:37:50 +0900

    ADD sendEmail class

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -1,33 +1,51 @@
 const nodemailer = require("nodemailer");
 
-// Create transporter with Gmail service
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL_ADDRESS, // Your Gmail email address
-    pass: process.env.EMAIL_PASSCODE, // Your Gmail app password (generated in Gmail settings)
-  },
-});
+module.exports = class EmailHandler {
+  constructor(userDetails) {
+    this.to = userDetails.emailAddress;
+    this.from = `MEET JAPAN ${process.env.EMAIL_ADDRESS}`;
+    this.subject = `${userDetails.name} ${userDetails.subject}`;
+    this.text = userDetails.text;
+  }
 
-// Define function for sending email
-async function sendEmail(clientEmail) {
-  // Create mail options object with details of the email to be sent
-  const mailOptions = {
-    from: process.env.EMAIL_ADDRESS, // Your Gmail email address
-    to: clientEmail.emailAddress, // Email address of the recipient
-    subject: clientEmail.emailSubject, // Subject of the email
-    text: clientEmail.emailText, // Body of the email
-  };
-
-  // Send email using the transporter
-  transporter
-    .sendMail(mailOptions)
-    .then((info) => {
-      console.log(`Email sent: ${info.response}`);
-    })
-    .catch((error) => {
-      console.log(`Error sending email: ${error}`);
+  // eslint-disable-next-line class-methods-use-this
+  createTransport() {
+    if (process.env.NODE_ENV === "production") {
+      // Sendgrid
+      // return nodemailer.createTransport({
+      //   service: "SendGrid",
+      //   auth: {
+      //     user: process.env.SENDGRID_USERNAME,
+      //     pass: process.env.SENDGRID_PASSWORD,
+      //   },
+      // });
+      return 1;
+    }
+    // Create transporter with Gmail service
+    return nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_ADDRESS, // Your Gmail email address
+        pass: process.env.EMAIL_PASSCODE, // Your Gmail app password (generated in Gmail settings)
+      },
     });
-}
+  }
 
-module.exports = sendEmail;
+  async sendEmail(emailFrom, emailText) {
+    const mailOptions = {
+      from: emailFrom || this.from, // Your Gmail email address
+      to: this.to, // Email address of the recipient
+      subject: this.subject, // Subject of the email
+      text: emailText || this.text, // Body of the email
+    };
+
+    await this.createTransport().sendMail(mailOptions);
+  }
+
+  async sendNewUserWelcome() {
+    await this.sendEmail(
+      "Meet Japan",
+      "Welcome to Meet Japan, we hope you enjoy your time here!"
+    );
+  }
+};
