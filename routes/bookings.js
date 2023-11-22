c68c85b11c00f76137ed9a5279d6b94ecc2a621d
ZoranUTF8 <janjiczoran23@gmail.com>
2023-11-23 00:45:46 +0900

    add stripe setup

---
+++ routes/bookings.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const applicationMiddleware = require("../middleware/index");
+const bookingController = require("../controllers/bookingController");
+
+router.get(
+  "/checkout-session/:tourId",
+  applicationMiddleware.RouteProtect.protectedRoute,
+  applicationMiddleware.RoleRestrictedRoute.restrictTo("user", "tourguide"),
+  bookingController.getCheckoutSession
+);
+
+module.exports = router;
